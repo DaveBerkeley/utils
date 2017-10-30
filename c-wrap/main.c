75f23969d730d87cdb8ea8a72373aeb2f1038201
Dave Berkeley <una@rotwang.co.uk>
2017-10-30 09:02:42 +0000

    added in va-args macro

--- c-wrap/main.c
+++ c-wrap/main.c
@@ -1,6 +1,26 @@
 
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "args.h"
+
+void _variad(size_t argc, ...) {
+    va_list ap;
+    va_start(ap, argc);
+    for (int i = 0; i < (int) argc; i++) {
+        printf("%d ", va_arg(ap, int));
+    }
+    printf("\n");
+    va_end(ap);
+}
+
+void _variad(size_t argc, ...);
+#define variad(...) _variad(PP_NARG(__VA_ARGS__), __VA_ARGS__)
+
+
 int main()
 {
+    variad(2, 4, 6, 8, 10);
     return 0;
 }
 
