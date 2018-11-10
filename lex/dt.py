ccec33b71653780a20d13c797411793c382606a9
Dave Berkeley <via@rotwang.co.uk>
2018-11-10 18:00:54 +0000

    wip: collate regexes used to extract date/time fields

--- lex/dt.py
+++ lex/dt.py
@@ -22,20 +22,63 @@ import datetime
 re_hms = "(\d\d):(\d\d):(\d\d)"
 re_m = "^([A-Z][a-z][a-z])$"
 re_d = "^(\d+)$"
+re_y = "^(\d\d\d\d)$"
 re_ymd = "(\d\d\d\d)[/-](\d\d)[/-](\d\d)"
 re_z = "([+-]\d\d:\d\d)"
 re_us = "\.(\d\d\d\d\d\d)"
 re_ms = "\.(\d\d\d)"
 
-res = [
-    [   re_hms, "hms", ],
-    [   re_ymd, "ymd", ],
-    [   re_m, "Mon", ],
-    [   re_d, "dd", ],
-    [   re_z, "zone", ],
-    [   re_us, "us", ],
-    [   re_ms, "ms", ],
+res = {
+    "hms"   : [ re_hms, ],
+    "ymd"   : [ re_ymd, ],
+    "month" : [ re_m, ],
+    "day"   : [ re_d, ],
+    "year"  : [ re_y, ],
+    "zone"  : [ re_z, ],
+    "us"    : [ re_us, ],
+    "ms"    : [ re_ms, ],
+}
+
+priority = [ 
+    [ "ymd", "month", ],
+    [ "ymd", "day", ],
+    [ "hms", ],
+    [ "us", "ms", ],
+    [ "zone", ],
 ]
+
+def analyze(parts):
+    found = {}
+    for i, part in enumerate(parts):
+        seen = False
+        for section, (regex, ) in res.items():
+            # check for whole match
+            r = re.compile("^" + regex + "$")
+            match = r.match(part)
+            if match:
+                seen = True
+                found[section] = i, match.span(), len(match.groups())
+                continue
+            if regex[0] == "^":
+                continue
+            # check for part match
+            r = re.compile(regex)
+            match = r.search(part)
+            if match:
+                seen = True
+                found[section] = i, match.span(), len(match.groups())
+        if not seen:
+            # must find date/time value in first parts of the line
+            break
+
+    # remove duplicated (worse) matches
+    best = {}
+    for priorities in priority:
+        for p in priorities:
+            if p in found:
+                best[p] = found[p]
+                break
+    print best
 
 if __name__ == "__main__":
 
@@ -49,21 +92,6 @@ if __name__ == "__main__":
     for line in tests:
         parts = line.split()
         print parts
-        for i, part in enumerate(parts):
-            for regex, x in res:
-                #print i, part, regex
-                # check for whole match
-                r = re.compile("^" + regex + "$")
-                match = r.match(part)
-                if match:
-                    print "whole ", i, part, x
-                    continue
-                if regex[0] == "^":
-                    continue
-                r = re.compile(regex)
-                match = r.search(part)
-                if match:
-                    print "offset", i, match.span(), part, x
-
+        analyze(parts)
 
 # FIN
