e9d45b09e6863f72f630098f10e6b9015628852a
Dave Berkeley <via@rotwang.co.uk>
2018-12-02 12:15:47 +0000

    wip: more timed dev

--- timed/timed.c
+++ timed/timed.c
@@ -1,23 +1,49 @@
 
 #include <stdbool.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <assert.h>
+#include <string.h>
 
 #define ASSERT(x) assert(x)
 
-typedef pthread_mutex_t *mutex;
+typedef pthread_mutex_t Mutex;
 
-struct Waiter;
+static inline void lock(Mutex *mutex)
+{
+    if (mutex)
+    {
+        pthread_mutex_lock(mutex);
+        // TODO : check error
+    }
+}
+
+static inline void unlock(Mutex *mutex)
+{
+    if (mutex)
+    {
+        pthread_mutex_unlock(mutex);
+        // TODO : check error
+    }
+}
+
+struct Semaphore;
 
 typedef struct Waiter
 {
     //  Wait for timeout or post
+    Mutex *mutex;
+
+    sem_t sem;
 
     //  Used by Semaphore to list waiters
     struct Waiter *next_wait;
 
     //  Used by Timer to list timed waiters
     struct Waiter *next_timed;
+
+    //  Expiry time
+    struct timespec expire;
 }   Waiter;
 
     /*
@@ -41,20 +67,50 @@ static inline struct Waiter **pnext(Waiter *w, enum WaitType wait)
 
 void waiter_insert(Waiter **head, Waiter *w, enum WaitType wait)
 {
+    lock(w->mutex);
+
     Waiter **next = pnext(w, wait);
     *next = *head;
     *head = w;
+
+    unlock(w->mutex);
 }
 
 void waiter_append(Waiter **head, Waiter *w, enum WaitType wait)
 {
+    lock(w->mutex);
+
     while (*head)
     {
         head = pnext(w, wait);
     }
     *head = w;
     *pnext(w, wait) = 0;
+
+    unlock(w->mutex);
 }
+
+void waiter_remove(Waiter **head, Waiter *w, enum WaitType wait)
+{
+    lock(w->mutex);
+
+    for (; *head; head = pnext(w, wait))
+    {
+        Waiter *item = *head;
+
+        if (item == w)
+        {
+            Waiter **next = pnext(w, wait);
+            *head = *next;
+            *next = 0;
+            break;
+        }
+    }
+
+    unlock(w->mutex);
+}
+
+//  TODO : insert sorted
 
     /*
      *
@@ -63,13 +119,45 @@ void waiter_append(Waiter **head, Waiter *w, enum WaitType wait)
 typedef struct
 {
     //  Handle post()
-    //  has list of waiter objects waiting on this Semaphore
+
+
+    //  list of waiter objects waiting on this Semaphore
+    Waiter *waiters;
+    //  lock for the list
+    Mutex mutex;
 }   Semaphore;
+
+int semaphore_wait(Semaphore *s)
+{
+    Waiter w;
+
+    memset(& w, 0, sizeof(w));
+    w.mutex = & s->mutex;
+
+    sem_init(& w.sem, 0, 0);
+
+    // Append to S's list
+    waiter_append(& s->waiters, & w, WAIT_BLOCK);
+
+    sem_wait(& w.sem);
+
+    // Remove from the list
+    waiter_remove(& s->waiters, & w, WAIT_BLOCK);
+
+    sem_destroy(& w.sem);
+
+    return 1234; // TODO
+}
+
+    /*
+     *
+     */
 
 typedef struct
 {
     //  Single timer object
     //  Has list of all Waiter objects doing timed wait
+    Mutex mutex;
 }   Timer;
 
     /*
