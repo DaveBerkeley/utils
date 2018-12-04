5b937338fde71073d71b2104c274fdcc0b33fd5e
Dave Berkeley <una@rotwang.co.uk>
2018-12-04 19:55:48 +0000

    wip: test append

--- timed/main.cpp
+++ timed/main.cpp
@@ -16,6 +16,17 @@ static void** pnext_item(void *v)
     return (void**) & item->next;
 }
 
+static void init_item(Item *item, int n)
+{
+    item->i = n + 100;
+    item->j = n + 200;
+    item->next = 0;
+}
+
+    /*
+     *
+     */
+
 TEST(List, Stack)
 {
     int size;
@@ -27,14 +38,11 @@ TEST(List, Stack)
     Item *item = 0;
     int num = 100;
     Item items[num];
-    memset(& items, 0, sizeof(items));
 
-    //  Identify each item
+    //  Initialise each item
     for (int i = 0; i < num; i++)
     {
-        Item *item = & items[i];
-        item->i = 100 + i;
-        item->j = 200 + i;
+        init_item(& items[i], i);
     }
 
     //  Push onto stack
@@ -50,11 +58,13 @@ TEST(List, Stack)
     {
         item = (Item*) list_pop((void**) & head, pnext_item, 0);
         EXPECT_TRUE(item);
+        // check it is the correct item
         EXPECT_EQ(& items[i], item);
 
         size = list_size((void**) & head, pnext_item, 0);
         EXPECT_EQ(i, size);
 
+        // check the item is correct
         EXPECT_EQ(item->i, 100 + i);
         EXPECT_EQ(item->j, 200 + i);
         EXPECT_EQ(0, item->next);
@@ -64,6 +74,56 @@ TEST(List, Stack)
 
     item = (Item*) list_pop((void**) & head, pnext_item, 0);
     EXPECT_EQ(0, item);
+}
+
+    /*
+     *
+     */
+
+TEST(List, Append)
+{
+    int size;
+    Item *head = 0;
+
+    Item *item = 0;
+    int num = 100;
+    Item items[num];
+
+    //  Initialise each item
+    for (int i = 0; i < num; i++)
+    {
+        init_item(& items[i], i);
+    }
+
+    //  Append to stack
+    for (int i = 0; i < num; i++)
+    {
+        list_append((void**) & head, & items[i], pnext_item, 0);
+        size = list_size((void**) & head, pnext_item, 0);
+        EXPECT_EQ(i+1, size);
+    }
+
+    //  Pop off stack
+    for (int i = 0; i < num; i++)
+    {
+        item = (Item*) list_pop((void**) & head, pnext_item, 0);
+        EXPECT_TRUE(item);
+        // check it is the correct item
+        EXPECT_EQ(& items[i], item);
+
+        // check the item is correct
+        EXPECT_EQ(item->i, 100 + i);
+        EXPECT_EQ(item->j, 200 + i);
+        EXPECT_EQ(0, item->next);
+    }
+
+    EXPECT_EQ(0, head);
+
+    item = (Item*) list_pop((void**) & head, pnext_item, 0);
+    EXPECT_EQ(0, item);
+
+    size = list_size((void**) & head, pnext_item, 0);
+    EXPECT_EQ(0, size);
 }
 
 //  FIN
